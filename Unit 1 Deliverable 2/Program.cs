db98e9a6cac1fb7f3cad47324c0b53c9d1dad3f4
Aletia <a.holloway2789@gmail.com>
2020-12-26 11:33:40 -0500

    Initial Commit Lab 2

---
+++ Unit 1 Deliverable 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unit_1_Deliverable_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
